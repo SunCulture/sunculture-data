5416729ed74aa8b1b2722f4701bd6ae58e8b2a49
nyangweso-rodgers <rodgerso65@gmail.com>
2025-05-13 09:00:41 +0300

    update docusaurus.config.js

--- docs/docusaurus/docusaurus.config.js
+++ docs/docusaurus/docusaurus.config.js
@@ -13,6 +13,7 @@ const config = {
   title: "SunCulture",
   tagline: "Data Catalog",
   favicon: "img/favicon.ico",
+  trailingSlash: true,
 
   // Set the production url of your site here
   url: "https://sunculture.github.io",
