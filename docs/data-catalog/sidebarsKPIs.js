b09054970ae13ec6a06b126189a236dd373aed28
nyangweso-rodgers <rodgerso65@gmail.com>
2025-06-26 23:01:43 +0300

    update sidebarsKPIs.js with new folder structure

--- docs/data-catalog/sidebarsKPIs.js
+++ docs/data-catalog/sidebarsKPIs.js
@@ -2,37 +2,88 @@ const sidebars = {
   kpiSidebar: [
     {
       type: "doc",
-      id: "overview", // your overview page
+      id: "overview",
       label: "Overview",
     },
     {
       type: "category",
-      label: "KPIs by Department",
+      label: "Metrics",
       collapsible: true,
       collapsed: false,
       items: [
-        { type: "doc", id: "sales", label: "Sales" },
-        { type: "doc", id: "credit", label: "Credit" },
-        { type: "doc", id: "supply-chain", label: "Supply Chain" },
-        { type: "doc", id: "operations", label: "Operations" },
-        { type: "doc", id: "aftersales", label: "Aftersales" },
-
         {
-          type: "doc",
-          id: "customer-experience",
-          label: "Customer Experience",
+          type: "category",
+          label: "Operations",
+          items: [
+            { type: "doc", id: "Operations/sales", label: "Sales" },
+            { type: "doc", id: "Operations/credit", label: "Credit" },
+            {
+              type: "doc",
+              id: "Operations/supply-chain",
+              label: "Operations/Supply Chain",
+            },
+            { type: "doc", id: "Operations/operations", label: "Operations" },
+            { type: "doc", id: "Operations/carbon", label: "Carbon" },
+            { type: "doc", id: "Operations/aftersales", label: "After Sales" },
+            {
+              type: "doc",
+              id: "Operations/customer-experience",
+              label: "Customer Experience",
+            },
+          ],
         },
         {
-          type: "doc",
-          id: "business-development",
-          label: "Business Development",
+          type: "category",
+          label: "Growth",
+          items: [
+            {
+              type: "doc",
+              id: "Growth/business-development",
+              label: "Business Development",
+            },
+          ],
         },
-        { type: "doc", id: "marketing", label: "Marketing" },
-        { type: "doc", id: "finance", label: "Finance" },
-        { type: "doc", id: "esg", label: "ESG" },
-        { type: "doc", id: "carbon", label: "Carbon" },
-        { type: "doc", id: "product", label: "Product" },
-        { type: "doc", id: "people-culture", label: "People & Culture" },
+        {
+          type: "category",
+          label: "Finance",
+          items: [
+            { type: "doc", id: "Finance/finance", label: "Finance" },
+            { type: "doc", id: "Finance/esg", label: "ESG" },
+          ],
+        },
+        {
+          type: "category",
+          label: "Product",
+          items: [
+            { type: "doc", id: "Product/product", label: "Product" },
+            { type: "doc", id: "Product/engineering", label: "Engineering" },
+            { type: "doc", id: "Product/data", label: "Data" },
+            { type: "doc", id: "Product/product-ops", label: "Product Ops" },
+            { type: "doc", id: "Product/quality", label: "Quality" },
+          ],
+        },
+        {
+          type: "category",
+          label: "People & Culture",
+          items: [
+            {
+              type: "doc",
+              id: "PeopleAndCulture/people-culture",
+              label: "People & Culture",
+            },
+          ],
+        },
+        {
+          type: "category",
+          label: "External Affairs",
+          items: [
+            {
+              type: "doc",
+              id: "ExternalAffairs/external-affairs",
+              label: "External Affairs",
+            },
+          ],
+        },
       ],
     },
   ],
